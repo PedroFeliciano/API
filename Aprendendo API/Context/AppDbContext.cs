1d5a80417a9ec61fb4585e9b3138e95948fe924b
PedroFeliciano <pedrofeliciano647@gmail.com>
2022-08-13 11:49:09 -0300

    estudos api3

--- Aprendendo API/Context/AppDbContext.cs
+++ Aprendendo API/Context/AppDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Aprendendo_API.Context
+﻿using Aprendendo_API.NovaPasta3;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aprendendo_API.Context
 {
-    public class AppDbContext
-    {
+    public class AppDbContext : DbContext
+    { 
+        public AppDbContext(DbContextOptions<AppContext> options ) : base ( options )
+        {}
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
     }
+
 }
